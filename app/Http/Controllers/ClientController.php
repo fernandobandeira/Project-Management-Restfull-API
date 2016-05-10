0c69bd5e5fbae63680f5f00e1a371b30642c7552
Fernando Henrique Bandeira <fernando@movelaria.com.br>
2016-05-10 11:35:37 -0300

    Camada service

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,39 +3,82 @@
 namespace CodeProject\Http\Controllers;
 
 use CodeProject\Repositories\ClientRepository;
+use CodeProject\Services\ClientService;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    /*
+    /**
      * @var ClientRepository
      */
     private $repository;
 
-    public function __construct(ClientRepository $repository)
+    /**
+     * @var ClientService
+     */
+    private $service;
+
+    public function __construct(ClientRepository $repository, ClientService $service)
     {
         $this->repository = $repository;
+        $this->service = $service;
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index() {
         return $this->repository->all();
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request) {
-        return $this->repository->create($request->all());
+        return $this->service->create($request->all());
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id) {
         return $this->repository->find($id);
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id) {
-        return $this->repository->update($request->all(), $id);
+        return $this->service->update($request->all(), $id);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id) {
         if ($this->repository->delete($id))
-            return ['message' => 'Cliente deletado com sucesso'];
-        abort(500);
+            return [
+                'error' => false,
+                'message' => 'Cliente deletado com sucesso.'
+            ];
+        return [
+            'error' => true,
+                'message' => 'Não foi possível deletar o Cliente.'
+            ];
     }
 }
