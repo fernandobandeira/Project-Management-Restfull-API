d273ff62c93f0dc42bda6bb59e7ad598c3bfb390
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2016-07-02 20:59:04 -0300

    Fix id projeto na routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,7 +19,7 @@ Route::group(['middleware' => 'api'], function () {
     Route::group(['middleware' => 'oauth'], function() {
         Route::resource('client', 'ClientController', ['except'=>['create','edit']]);
         Route::resource('project', 'ProjectController', ['except'=>['create','edit']]);
-        Route::get('/project/{id}/members', 'ProjectController@members');
+        Route::get('/project/{project}/members', 'ProjectController@members');
         Route::resource('project.note', 'ProjectNoteController', ['except'=>['create','edit']]);
         Route::resource('project.task', 'ProjectTaskController', ['except'=>['create','edit']]);
     });
